5e449cd2cff55d8dfb5a6835acc04431a8fb0d8d
jjosecodes <josejob123121@gmail.com>
2025-02-03 23:03:36 -0600

    notes v1

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from fastapi  import FastAPI
+from fastapi import FastAPI
+from fastapi import Body
 
-app = FastAPi()
 
-@app.get("/")   
+app = FastAPI()
+
+# request Get method url: "/"
+@app.get("/") 
+async def root():
+    return {"message": "Welcome to the API !"}
+
+@app.get("/posts")
+def get_posts():
+    return{"data": "These are your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return{"new_post": f"title {payload}" } 
